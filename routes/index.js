7a11bbcfd9172980974ab049271de188f2a4fb9b
Felipe Keller Braz <fbraz3@gmail.com>
2022-06-21 21:20:07 -0300

    added parameters to adsense and adwords

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   let the_host = req.headers['x-forwarded-host'] || req.headers.host
-  res.render('index', {host: the_host});
+  let config_gtag = process.env.CONFIG_GTAG
+  let config_adsense = process.env.CONFIG_ADSENSE
+
+  res.render('index', {
+    host: the_host,
+    config_gtag: config_gtag,
+    config_adsense: config_adsense
+  });
 });
 
 module.exports = router;
