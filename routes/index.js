ceedef7f5b45f4606a71675733e599630bae1fa4
Felipe Keller Braz <fbraz3@gmail.com>
2023-03-11 15:54:05 -0300

    added config_repo variable

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ router.get('/', function(req, res, next) {
   let the_host = req.headers['x-forwarded-host'] || req.headers.host
   let config_gtag = process.env.CONFIG_GTAG
   let config_adsense = process.env.CONFIG_ADSENSE
+  let config_repo = process.env.CONFIG_REPO || "https://github.com/fbraz3/ansible-vault-tool"
 
   res.render('index', {
     host: the_host,
     config_gtag: config_gtag,
     config_adsense: config_adsense
+    config_repo: config_repo
   });
 });
 
